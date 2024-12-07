40f3333649c088a660a53b15cd153b7442cf94c4
Daniel Manila <daniel.manila@willowtreeapps.com>
2024-12-07 11:32:33 -0500

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -1,4 +1,11 @@
+from collections import defaultdict
+from typing import Mapping
 import utils
+
+
+def print_map(map):
+    for line in map:
+        print(line)
 
 
 def find_block(map, start_loc, dir):
@@ -50,6 +57,59 @@ def part1(map):
     return len(locations)
 
 
+def is_map_loop(map, start_loc, start_dir):
+    current_loc = start_loc
+    current_dir = start_dir
+
+    paths_for_dir = defaultdict(set)
+
+    off_map = False
+    while not off_map:
+        off_map, paths, current_loc = find_block(map, current_loc, current_dir)
+
+        paths_for_dir[current_dir] = paths_for_dir[current_dir].union(paths)
+
+        current_dir = turn_right(current_dir)
+
+        if current_loc in paths_for_dir[current_dir]:
+            return True
+    return False
+
+
+def find_map_paths(map):
+    current_loc = find_start(map)
+    current_dir = (0, -1)
+
+    off_map = False
+    locations = set()
+    while not off_map:
+        off_map, paths, current_loc = find_block(map, current_loc, current_dir)
+        locations = locations.union(paths)
+        current_dir = turn_right(current_dir)
+
+    return locations
+
+
+def part2(map):
+    current_loc = find_start(map)
+    current_dir = (0, -1)
+
+    possible_locations = find_map_paths(map)
+    total_loops = 0
+    i = 0
+    for x, y in possible_locations:
+        i += 1
+        if i % 1000 == 0:
+            print(f"{i}/{len(possible_locations)}")
+        if map[y][x] == ".":
+            new_map = map.copy()
+            new_map[y] = new_map[y][:x] + "#" + new_map[y][x + 1 :]
+            if is_map_loop(new_map, current_loc, current_dir):
+                total_loops += 1
+
+    return total_loops
+
+
 example = """....#.....
 .........#
 ..........
@@ -61,5 +121,5 @@ example = """....#.....
 #.........
 ......#..."""
 
-# print(part1(example.split("\n")))
-print(part1(utils.get_day_lines(6)))
+# print(part2(example.split("\n")))
+print(part2(utils.get_day_lines(6)))
