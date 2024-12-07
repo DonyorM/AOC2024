1c45eda1172fa6cc1d3d2d299b583ee891162296
Daniel Manila <daniel.manila@willowtreeapps.com>
2024-12-07 11:51:03 -0500

    Day 7 Part 2

--- day7.py
+++ day7.py
@@ -1,17 +1,18 @@
+import math
 import utils
 
 
-def is_row_possible(total: int, next_num: int, rest_nums: list[int]) -> bool:
-    if next_num > total:
+def is_row_possible_part1(total: int, current_num: int, rest_nums: list[int]) -> bool:
+    if current_num > total:
         return False
     if len(rest_nums) == 0:
-        return next_num == total
+        return current_num == total
 
     operate_num = rest_nums[0]
     rest_nums = rest_nums[1:]
-    if is_row_possible(total, next_num + operate_num, rest_nums):
+    if is_row_possible_part1(total, current_num + operate_num, rest_nums):
         return True
-    return is_row_possible(total, next_num * operate_num, rest_nums)
+    return is_row_possible_part1(total, current_num * operate_num, rest_nums)
 
 
 def parse_input(lines: list[str]):
@@ -25,7 +26,42 @@ def parse_input(lines: list[str]):
 def part1(input: list[str]) -> int:
     rows = parse_input(input)
     return sum(
-        [total for total, rest in rows if is_row_possible(total, rest[0], rest[1:])]
+        [
+            total
+            for total, rest in rows
+            if is_row_possible_part1(total, rest[0], rest[1:])
+        ]
+    )
+
+
+def join_operator(num1: int, num2: int) -> int:
+    count = math.floor(math.log10(num2)) + 1
+    return (num1 * (10**count)) + num2
+
+
+def is_row_possible_part2(total: int, current_num: int, rest_nums: list[int]) -> bool:
+    if current_num > total:
+        return False
+    if len(rest_nums) == 0:
+        return current_num == total
+
+    operate_num = rest_nums[0]
+    rest_nums = rest_nums[1:]
+    if is_row_possible_part2(total, current_num + operate_num, rest_nums):
+        return True
+    if is_row_possible_part2(total, join_operator(current_num, operate_num), rest_nums):
+        return True
+    return is_row_possible_part2(total, current_num * operate_num, rest_nums)
+
+
+def part2(input: list[str]) -> int:
+    rows = parse_input(input)
+    return sum(
+        [
+            total
+            for total, rest in rows
+            if is_row_possible_part2(total, rest[0], rest[1:])
+        ]
     )
 
 
@@ -39,4 +75,5 @@ example = """190: 10 19
 21037: 9 7 18 13
 292: 11 6 16 20"""
 
-print(part1(utils.get_day_lines(7)))
+# print(part2(utils.example_lines(example)))
+print(part2(utils.get_day_lines(7)))
