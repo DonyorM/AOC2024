38453134c4f37ca2d532530a4601d7a5f9f3c8d3
Daniel Manila <daniel.manila@willowtreeapps.com>
2024-12-09 11:17:32 -0500

    Day 8 Part 2

--- day8.py
+++ day8.py
@@ -50,6 +50,40 @@ def part1(map):
     return len(antinodes)
 
 
+def part2_get_antinodes(antenna_locs: list[tuple[int, int]], max_x: int, max_y: int):
+    combinations = itertools.product(antenna_locs, repeat=2)
+    locs = set()
+    for (x1, y1), (x2, y2) in combinations:
+        if x1 == x2 and y1 == y2:
+            continue
+
+        x_diff = x1 - x2
+        y_diff = y1 - y2
+        loc = (x1 + x_diff, y1 + y_diff)
+        while loc_on_map(*loc, max_x, max_y):
+            locs.add(loc)
+            loc = (loc[0] + x_diff, loc[1] + y_diff)
+
+        loc = (x1 - x_diff, y1 - y_diff)
+        while loc_on_map(*loc, max_x, max_y):
+            locs.add(loc)
+            loc = (loc[0] - x_diff, loc[1] - y_diff)
+
+    return locs
+
+
+def part2(map):
+    antennas = parse_map(map)
+    max_y = len(map) - 1
+    max_x = len(map[0]) - 1
+    antinodes = set()
+
+    for antenna_locs in antennas.values():
+        antinodes.update(part2_get_antinodes(antenna_locs, max_x, max_y))
+
+    return len(antinodes)
+
+
 example = """............
 ........0...
 .....0......
@@ -64,5 +98,5 @@ example = """............
 ............"""
 
 # 335 is too high
-# print(part1(utils.example_lines(example)))
-print(part1(utils.get_day_lines(8)))
+# print(part2(utils.example_lines(example)))
+print(part2(utils.get_day_lines(8)))
